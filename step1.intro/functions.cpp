693d6f5941fc66164b96116dfd397025620916b0
Ilya Sokolov <developer.sokolov@gmail.com>
2022-06-20 00:23:09 +0300

    add MAX macros

--- step1.intro/functions.cpp
+++ step1.intro/functions.cpp
@@ -1,9 +1,16 @@
 #include "iostream"
+/* присваивает r максимум из x и y */
+#define MAX(x, y, r)  \
+{                     \
+int xx = x;           \
+int yy = y;           \
+r = xx > yy?xx:yy;    \
+}
+
 // определите только функцию power_recurion, где
 //    x - число, которое нужно возвести в степень
 //    p - степень, в которую нужно возвести x
 //
-
 int power(int x, unsigned p) {
     if (p == 0) return 1;
     int answer = x;
@@ -22,5 +29,12 @@ int main() {
     std::cout << "power_recurion(13,0) == " << power_recurion(13, 0) << std::endl;
     std::cout << "power(5,3) == " << power_recurion(5, 3) << std::endl;
     std::cout << "power(4,4) == " << power_recurion(4, 4) << std::endl;
+    int a = 10;
+    int b = 20;
+    int c = 0;
+    MAX(a, b, c);
+    std::cout << "C == " << c << std::endl;
+    MAX(a += b, b, c);
+    std::cout << "C == " << c << std::endl;
     return 0;
 }
