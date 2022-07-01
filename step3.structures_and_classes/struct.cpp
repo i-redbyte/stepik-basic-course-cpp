11926b1faefc47f4fa539faa98e67c9fa23177dd
Ilya Sokolov <developer.sokolov@gmail.com>
2022-07-01 17:50:37 +0300

    step3 task3 create append string function

--- step3.structures_and_classes/struct.cpp
+++ step3.structures_and_classes/struct.cpp
@@ -39,13 +39,63 @@ struct String {
 
     size_t size;
     char *str;
+
+/** Реализованные в предыдущих заданиях конструкторы и деструктор берут на себя работу по управлению ресурсами.
+ *  Теперь можно дополнить структуру String различными полезными методами.
+ *  Для работы со строками можно придумать множество полезных методов (подумайте, какие методы пригодились бы вам,
+ *  и чего вам не хватает для их реализации). Примером такого метода может послужить метод append — он добавляет копию
+ *  строки-аргумента в конец текущей строки (т.е. в конец строки, у которой он был вызван).
+    String s1("Hello,");
+    String s2(" world!");
+    s1.append(s2); // теперь s1 хранит "Hello, world!"
+
+ * Ваша задача реализовать метод append.
+ * При выполнении задания будьте аккуратны при работе с памятью — при вызове метода не должно возникать утечек памяти.
+ * Кроме того, не забудьте, что size хранит размер без учета завершающего 0 символа.
+ * Кроме того, ваша реализация должна корректно работать в следующем тесте:
+        String s("Hello");
+        s.append(s); // теперь s хранит "HelloHello"
+ * */
+    void append(String &other) {
+        size_t new_size = size + other.size;
+        char* new_ptr = new char [new_size + 1];
+        strcpy(new_ptr, str);
+        strcat(new_ptr + size, other.str);
+        new_ptr[new_size] = '\0';
+        delete[] str;
+        str = new_ptr;
+        size = strlen(str);
+
+//        size_t new_size = this->size + other.size;
+//        char *new_str = new char[new_size];
+//        for (size_t i = 0; i < this->size; ++i) new_str[i] = this->str[i];
+//        for (size_t i = 0; i < other.size; ++i) new_str[this->size + i] = other.str[i];
+//        delete[] this->str;
+//        this->str = new char[new_size + 1];
+//        strcpy(this->str, new_str);
+//        this->str[new_size] = '\0';
+//        this->size = new_size;
+//        delete[] new_str;
+    }
 };
 
 int main() {
-    String s("Lenin");
+    String s("Stroustrup");
     std::cout << "Length = " << s.size << " for string: " << s.str << std::endl;
     String s1(5, '*');
     std::cout << "String for symbol: " << s1.str << std::endl;
-
+    s1.append(s1);
+    std::cout << "s1 == " << s1.str << std::endl;
+    s1.append(s);
+    std::cout << "s1 == " << s1.str << std::endl;
+    s1.append(s1);
+    std::cout << "s1 == " << s1.str << std::endl;
+    String s2("");
+    std::cout << "s2 == " << s2.str << std::endl;
+    s2.append(s2);
+    std::cout << "s2 == " << s2.str << std::endl;
+    String ss("Hello");
+    ss.append(ss);
+    std::cout << "ss == " << ss.str << std::endl;
     return 0;
 }
